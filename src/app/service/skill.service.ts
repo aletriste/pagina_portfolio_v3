13f135f64afb8060d51ffa956526911550db719e
aletriste <atrsitezza@gmail.com>
2022-07-28 15:57:40 -0300

    mimodificaciones en el edit servicio skill

--- src/app/service/skill.service.ts
+++ src/app/service/skill.service.ts
@@ -3,6 +3,7 @@ import { Skill } from '../interfaces';
 import { SKILLS } from '../data';
 import { Observable, of } from 'rxjs';
 import {HttpClient, HttpHandler, HttpHeaders} from '@angular/common/http'
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 
 const httpOptions = {
   headers : new HttpHeaders({
@@ -30,4 +31,9 @@ export class SkillService {
     const url = `${this.apiUrl}/delete${skill.id}`
     return this.http.delete<any>(this.apiUrl + `/delete/${skill.id}`)
   }
+
+  editSkill(skill:Skill):Observable<Skill>{
+    return this.http.post<Skill>(this.apiUrl + '/edit', skill, httpOptions)
+  }
+  
 }
