8a9c903211ae0524e4783eb00bac31e99959178a
aletriste <atrsitezza@gmail.com>
2022-07-29 18:40:12 -0300

    agregados los *ngif faltantes

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AboutService } from 'src/app/service/about.service';
 import { About } from 'src/app/interfaces';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-about',
@@ -9,11 +10,17 @@ import { About } from 'src/app/interfaces';
 })
 export class AboutComponent implements OnInit {
   about : About[] = []
+  isLogged = false;
   
-  constructor(private aboutService : AboutService) { }
+  constructor(private aboutService : AboutService, private tokenService : TokenService) { }
 
   ngOnInit(): void {
     this.aboutService.getAbout().subscribe((about)=>this.about = about)
+    if(this.tokenService.getToken()){
+      this.isLogged=true;
+    }else{
+      this.isLogged = false;
+    }
   }
 
   editAbout(about:About){
